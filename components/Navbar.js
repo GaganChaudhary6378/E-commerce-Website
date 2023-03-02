5ce79355ef9ac33c64a5f36dcde9782a26f71a38
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-02 21:52:25 +0530

    background added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,24 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineShoppingCart } from 'react-icons/Ai';
+
 export default function Navbar(){
     return (
-        <div className="flex flex-col md:flex-row md:justify-start justify-center items-center">
+        <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2">
             <div className="logo mx-5">
                 <Image width={170} height={40} src="/logoName.png"></Image>
             </div>
             <div className="nav">
-                <ul className="flex items-centre gap-4 mt-1 font-bold">
+                <ul className="flex items-centre  md:-mt-2 gap-4 font-bold mt-4">
                     <Link href={'/'}><li>Tshirts</li></Link>
                     <Link href={'/'}><li>Hoodies</li></Link>
                     <Link href={'/'}><li>Stickers</li></Link>
                     <Link href={'/'}><li>Mugs</li></Link>
                 </ul>
             </div>
-            <div className="cart absolute right-0 top-0 mx-5 md:mt-1 font-bold">
-                <button>Cart</button>
+            <div className="cart absolute right-0 top-2 mx-5 md:mt-2 font-bold">
+                <AiOutlineShoppingCart className="text-xl md:text-2xl"/>
             </div>
         </div>
     )
