202a18f8f6eb5ddf3e0e0d07fa580e2d6a96c412
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-02 21:57:08 +0530

    -

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { AiOutlineShoppingCart } from 'react-icons/Ai';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 export default function Navbar(){
     return (
