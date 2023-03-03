c43eab9e86e1bdcb0c54f987ef601d40291c4537
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-03 21:18:45 +0530

    added margins

--- src/pages/hoodies.js
+++ src/pages/hoodies.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 export default function hoodies() {
   return (
-    <div>
+    <div className="md:ml-12 ">
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
