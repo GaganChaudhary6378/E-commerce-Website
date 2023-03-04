32af4997c34a30efd0c939c5dfccd035cd9cf62c
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-04 19:57:23 +0530

    added pincode check

---
+++ src/pages/api/pincode.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+    res.status(200).json([321001,110003,560017])
+  }
+  
--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,6 +1,23 @@
 import React from "react";
 
 export default function slug() {
+  const [pin, setPin] = React.useState();
+  const [service, setService] = React.useState();
+
+  async function checkServicibility() {
+    let pins = await fetch("http://localhost:3001/api/pincode");
+    let pinJson = await pins.json();
+    console.log(pinJson)
+    if (pinJson.includes(parseInt(pin))) {
+      setService(true);
+    } else {
+      setService(false);
+    }
+  }
+  function onChangePin(e){
+    console.log(e.target.value)
+    setPin(e.target.value);
+  }
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
@@ -13,10 +30,10 @@ export default function slug() {
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                BRAND NAME
+                LoopCart
               </h2>
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                The Catcher in the Rye
+                Shop in loop with loop cart
               </h1>
               <div className="flex mb-4">
                 <span className="flex items-center">
@@ -158,9 +175,12 @@ export default function slug() {
               </div>
               <div className="flex">
                 <span className="title-font font-medium text-2xl text-gray-900">
-                ₹580.00
+                  ₹580.00
                 </span>
-                <button className="flex ml-14 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
+                <button className="flex ml-4 md:ml-8 text-white bg-pink-500 border-0 py-2 px-2 md:px-8 focus:outline-none hover:bg-pink-600 rounded">
+                  Buy Now
+                </button>
+                <button className="flex ml-2 md:ml-4 text-white bg-pink-500 border-0 py-2 px-2 md:px-8 focus:outline-none hover:bg-pink-600 rounded">
                   Add to Cart
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
@@ -176,6 +196,26 @@ export default function slug() {
                   </svg>
                 </button>
               </div>
+              <div className="pin mt-6 flex space-x-2 text-sm">
+                <input
+                  className="px-2 border-2 border-gray-400 rounded-md"
+                  placeholder="Enter your Pincode"
+                  onChange={onChangePin}
+                  type="text"
+                />
+                <button
+                  onClick={checkServicibility}
+                  className="flex ml-14 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
+                >
+                  Check
+                </button>
+              </div>
+              {!service && service!=null && <div className="text-red-700 text-sm mt-3">
+                Sorry! We do not deliver to this pincode.
+              </div>}
+              {service && service!=null && <div className="text-green-700 text-sm mt-3">
+                Yay! This pinocode is serviceable.
+              </div>}
             </div>
           </div>
         </div>
