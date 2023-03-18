3f8384c57b01fcffe220d6fa59453752c0f679ce
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-18 23:27:52 +0530

    --

--- src/pages/order.js
+++ src/pages/order.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function order(){
     return (
         <div>
-            That the order page here you can see the orders that you have made.
+            That's the order page here you can see the orders that you have made.
         </div>
     )
 }
