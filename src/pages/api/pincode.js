a463e4165b2f697ae877e69d72ef9283aa0471b4
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-29 00:44:19 +0530

    improved hoodies page

--- src/pages/api/pincode.js
+++ src/pages/api/pincode.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    res.status(200).json([321001,110003,560017,632014,110034])
+    res.status(200).json([321001,110003,560017,632014,110034,812006])
   }
   
