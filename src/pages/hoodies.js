d60be67b4ca591b9c0d7f7e60776bd5d3d7c32e7
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-04-01 22:43:01 +0530

    jwt

--- src/pages/hoodies.js
+++ src/pages/hoodies.js
@@ -9,7 +9,7 @@ export default function hoodies({ products }) {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
-          {Object.keys(products).length===0 && <p>All the hoodies are out of stock, please stay tuned for new product.</p>}
+          {Object.keys(products).length===0 && <p>All the hoodies are out of stock, wait or please stay tuned for new product.</p>}
             {Object.keys(products).map((item) => {
               console.log(products);
               return (
