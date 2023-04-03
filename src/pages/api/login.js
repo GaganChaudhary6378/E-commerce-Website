4df8b51ee08e08229dabd2b32d0191504b4a76cf
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-04-03 23:17:54 +0530

    expires in

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -13,7 +13,9 @@ const handler = async (req, res) => {
     // console.log(decryptedPass)
     if (user) {
       if (req.body.email == user.email && req.body.password == decryptedPass) {
-        var token = jwt.sign({email: user.email, name: user.name }, 'jwtsecret');
+        var token = jwt.sign({email: user.email, name: user.name }, 'jwtsecret',{
+          expiresIn: "1d"
+        });
         res.status(200).json({success: true,token});
       } else {
         res
