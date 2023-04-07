870e55873a68dd4ebef977957cc23cb65aac344e
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-04-07 19:03:58 +0530

    added paytm integration

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,15 +2,78 @@ import React from "react";
 import Link from "next/link";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { AiFillMinusCircle } from "react-icons/ai";
+import Head from "next/head";
 import { BsFillBagCheckFill } from "react-icons/bs";
+import Script from "next/script";
+
 export default function checkout({
   cart,
   subTotal,
   removeFromCart,
   addToCart,
 }) {
+
+  async function initiatePayment()  {
+    // Here we are getting the transaction token
+  let oid = Math.floor(Math.random() * Date.now());
+
+
+    const data = { cart, subTotal, oid, email: "email" };
+
+    let a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pretransaction`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    let txnRes = await a.json();
+    // console.log(txnToken);
+    let txnToken=txnRes.txnToken;
+    // const data = { username: "example" };
+    // postJSON(data);
+    var config = {
+      "root": "",
+      "flow": "DEFAULT",
+      "data": {
+        "orderId": oid /* update order id */,
+        "token": txnToken /* update token value */,
+        "tokenType": "TXN_TOKEN",
+        "amount": subTotal /* update amount */,
+      },
+      "handler": {
+        "notifyMerchant": function (eventName, data) {
+          console.log("notifyMerchant handler function called");
+          console.log("eventName => ", eventName);
+          console.log("data => ", data);
+        },
+      },
+    };
+
+    window.Paytm.CheckoutJS.init(config)
+      .then(function onSuccess() {
+        // after successfully updating configuration, invoke JS Checkout
+        window.Paytm.CheckoutJS.invoke();
+      })
+      .catch(function onError(error) {
+        console.log("error => ", error);
+      })
+  }
   return (
     <div className="container m-auto">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"
+        />
+      </Head>
+      <Script
+        type="application/javascript"
+        src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}
+        crossorigin="anonymous"
+      />
+
       <h1 className="font-bold text-3xl my-8 text-center">Checkout</h1>
       <h2 className="font-bold text-xl mx-11">1. Delivery Details</h2>
       <div className="mx-auto flex my-4 mr-10 ml-10">
@@ -124,7 +187,9 @@ export default function checkout({
             return (
               <li key={k}>
                 <div className="pl-4 item flex my-5">
-                  <div className=" font-semibold">{cart[k].name}({cart[k].size}/{cart[k].variant})</div>
+                  <div className=" font-semibold">
+                    {cart[k].name}({cart[k].size}/{cart[k].variant})
+                  </div>
                   <div className="flex font-semibold items-center justify-center w-1/3">
                     <AiFillMinusCircle
                       onClick={() => {
@@ -164,7 +229,10 @@ export default function checkout({
         </span>
       </div>
       <div className="mx-10">
-        <button className="flex mt-16 text-white bg-pink-500 border-0 py-2 px-4 focus:outline-none hover:bg-pink-600 rounded text-lg">
+        <button
+          onClick={initiatePayment}
+          className="flex mt-16 text-white bg-pink-500 border-0 py-2 px-4 focus:outline-none hover:bg-pink-600 rounded text-lg"
+        >
           <BsFillBagCheckFill className="m-1" />
           Pay ₹{subTotal}
         </button>
