e942fcd50f7b473d2348f0527bafde9ce3214028
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-04-10 23:40:56 +0530

    added key

--- src/pages/api/posttransaction.js
+++ src/pages/api/posttransaction.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import React from "react"
+
 export default function handler(req, res) {
     res.status(200).json({body:req.body})
   }
